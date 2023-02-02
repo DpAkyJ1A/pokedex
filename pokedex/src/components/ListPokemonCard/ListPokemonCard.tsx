a970e613aa676eaac4fded5d0418685c5d5951a9
Aleksey Zvagolsky <alekseyzvagolsky@gmail.com>
2023-02-02 23:27:33 +0200

    feat: implement display of empty pokemon list

--- pokedex/src/components/ListPokemonCard/ListPokemonCard.tsx
+++ pokedex/src/components/ListPokemonCard/ListPokemonCard.tsx
@@ -6,6 +6,7 @@ interface IListPokemonCard {
 }
 
 export default function ListPokemonCard({ pokemons }: IListPokemonCard) {
+  if (pokemons.length === 0) return <h2 style={{ fontSize: '7rem' }}>{'¯\\_(ツ)_/¯'}</h2>;
   return (
     <div className="card-list">
       {pokemons.map((pokemon: IPokemon) => (
